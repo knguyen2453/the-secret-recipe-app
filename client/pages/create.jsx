dd98cb8803264add77edee9c1f8157f43357d357
knguyen2453 <yeaitskevinn@yahoo.com>
2021-02-16 19:50:10 -0800

    Setup Basic Routing

---
+++ client/pages/create.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Create() {
+  return <h1 className="text-center">Create A Recipe</h1>;
+}
---
+++ client/pages/recipes.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Recipes() {
+  return <h1 className="text-center">Search For A Recipe</h1>;
+}
